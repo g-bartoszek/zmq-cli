f47cf137ffe453ec80c0f57a6e16468306dd7280
Grzegorz <grzegorz.bartoszek@thaumatec.com>
2019-10-11 11:52:54 +0200

    bind/connect

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,30 @@ use clap::{App, SubCommand, AppSettings, Arg, ArgMatches};
 use std::thread::sleep;
 use std::time::Duration;
 
-fn subscribe_and_listen(topic: Option<&str>, address: &str) {
-    println!("Listening {:?}", address);
+fn create_socket(ctx: &zmq::Context, parameters: SocketParameters) -> zmq::Socket {
+    let socket = ctx.socket(match parameters.socket_type {
+        "PUB" => zmq::PUB,
+        "SUB" => zmq::SUB,
+        "PUSH" => zmq::PUSH,
+        "PULL" => zmq::PULL,
+        _ => zmq::PULL,
+    }).unwrap();
+
+    match parameters.association_type {
+        AssociationType::Connect => socket.connect(parameters.address).unwrap(),
+        AssociationType::Bind => socket.bind(parameters.address).unwrap(),
+    };
+
+    socket
+}
+
+fn subscribe_and_listen(topic: Option<&str>, parameters: SocketParameters) {
+    println!("Listening {:?}", parameters.address);
     let ctx = zmq::Context::new();
-    let socket = ctx.socket(zmq::SUB).unwrap();
+
+    let socket = create_socket(&ctx, parameters);
 
     socket.set_subscribe(topic.unwrap_or("").as_bytes()).unwrap();
-    socket.connect(address).unwrap();
 
     loop {
         let msg = socket.recv_msg(0);
@@ -16,22 +33,20 @@ fn subscribe_and_listen(topic: Option<&str>, address: &str) {
     }
 }
 
-fn publish(address: &str, message: &str) {
-    println!("Sending to {:?}", address);
+fn publish(parameters: SocketParameters, message: &str) {
+    println!("Sending to {:?}", parameters.address);
     let ctx = zmq::Context::new();
-    let socket = ctx.socket(zmq::PUB).unwrap();
-    socket.connect(address).unwrap();
+    let socket = create_socket(&ctx, parameters);
+
     sleep(Duration::from_millis(100));
     socket.send(message, 0).unwrap();
 }
 
 
-fn pull(address: &str) {
-    println!("Listening {:?}", address);
+fn pull(parameters: SocketParameters) {
+    println!("Listening {:?}", parameters.address);
     let ctx = zmq::Context::new();
-    let socket = ctx.socket(zmq::PULL).unwrap();
-
-    socket.connect(address).unwrap();
+    let socket = create_socket(&ctx, parameters);
 
     loop {
         let msg = socket.recv_msg(0);
@@ -39,11 +54,10 @@ fn pull(address: &str) {
     }
 }
 
-fn push(address: &str, message: &str) {
-    println!("Sending to {:?}", address);
+fn push(parameters: SocketParameters, message: &str) {
+    println!("Sending to {:?}", parameters.address);
     let ctx = zmq::Context::new();
-    let socket = ctx.socket(zmq::PUSH).unwrap();
-    socket.bind(address).unwrap();
+    let socket = create_socket(&ctx, parameters);
     sleep(Duration::from_millis(100));
     socket.send(message, 0).unwrap();
 }
@@ -63,8 +77,36 @@ fn socket_type_arg(values: &[&'static str]) -> Arg<'static, 'static> {
         .default_value(values[0])
 }
 
-fn extract_common_parameters<'a>(matches: &'a ArgMatches) -> (&'a str, &'a str) {
-    (matches.value_of("address").unwrap(), matches.value_of("socket type").unwrap())
+enum AssociationType {
+    Bind,
+    Connect
+}
+
+struct SocketParameters<'a>
+{
+    address: &'a str,
+    socket_type: &'a str,
+    association_type: AssociationType
+}
+
+fn extract_common_parameters<'a>(matches: &'a ArgMatches) -> SocketParameters<'a> {
+    let socket_type = matches.value_of("socket type").unwrap();
+    let a = match matches.value_of("bind or connect") {
+        Some("bind") => AssociationType::Bind,
+        Some("connect") => AssociationType::Connect,
+        _ => match socket_type {
+            "PUSH" => AssociationType::Bind,
+            "PUB" => AssociationType::Bind,
+            "SUB" => AssociationType::Connect,
+            "PULL" => AssociationType::Connect,
+            _ => AssociationType::Connect,
+        }
+    };
+
+    SocketParameters{
+        address: matches.value_of("address").unwrap(),
+        socket_type,
+        association_type: a}
 }
 
 fn main() {
@@ -77,27 +119,29 @@ fn main() {
                 .long("message")
                 .short("m")
                 .takes_value(true)
-                .required(true)))
+                .required(true))
+            .arg(Arg::with_name("bind or connect").possible_values(&["bind", "connect"])))
         .subcommand(SubCommand::with_name("listen")
             .arg(address_arg())
             .arg(socket_type_arg(&["PULL", "SUB", ]))
+            .arg(Arg::with_name("bind or connect").possible_values(&["bind", "connect"]))
             .arg(Arg::with_name("topic")
                 .long("topic")
                 .short("t")
                 .takes_value(true)))
         .get_matches();
 
-    println!("{:?}", matches);
+    //println!("{:?}", matches);
 
     match matches.subcommand() {
         ("send", args) => {
             match args {
                 Some(matches) => {
-                    let (address, socket_type) = extract_common_parameters(matches);
+                    let parameters = extract_common_parameters(matches);
                     let message = matches.value_of("message").unwrap();
-                    match socket_type {
-                        "PUB" => { publish(address, message); },
-                        "PUSH" => { push(address, message); }
+                    match parameters.socket_type {
+                        "PUB" => { publish(parameters, message); },
+                        "PUSH" => { push(parameters, message); }
                         _ => {}
                     }
                 }
@@ -107,12 +151,12 @@ fn main() {
         ("listen", args) => {
             match args {
                 Some(matches) => {
-                    let (address, socket_type) = extract_common_parameters(matches);
-                    match socket_type {
+                    let parameters = extract_common_parameters(matches);
+                    match parameters.socket_type {
                         "SUB" => {
-                            subscribe_and_listen(matches.value_of("topic"), address);
+                            subscribe_and_listen(matches.value_of("topic"), parameters);
                         },
-                        "PULL" => { pull(address); },
+                        "PULL" => { pull(parameters); },
                         _ => {}
                     }
                 }
