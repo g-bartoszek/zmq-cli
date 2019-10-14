487d6d8cde6b3639b94c3d84f4440b030a20c092
Grzegorz <grzegorz.bartoszek@thaumatec.com>
2019-10-14 10:34:29 +0200

    integration test passes

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,21 +1,28 @@
-use std::process::Command;
+use std::process::{Command, Stdio};
 use assert_cmd::prelude::*;
 use std::time::Duration;
 
 
 #[test]
 fn test_push_pull_send_listen() {
-    let listen = std::thread::spawn(||{
-        let o = Command::cargo_bin("rzmq").unwrap()
-            .args(&["listen", "--address", "tcp://127.0.0.1:5559", "--type", "PULL", "bind"]).unwrap();
-        println!("Output: {:?}", o);
-    });
+    let test_message = "TEST MESSAGE 122345";
 
-    std::thread::sleep(Duration::from_millis(100));
+    let mut listener = Command::cargo_bin("rzmq").unwrap()
+        .args(&["listen", "--address", "tcp://127.0.0.1:5559", "--type", "PULL", "bind"])
+        .stdout(Stdio::piped()).spawn().unwrap();
 
-    let o = Command::cargo_bin("rzmq").unwrap()
-        .args(&["send", "--message", "TEST1", "--address", "tcp://127.0.0.1:5559", "--type", "PUSH", "connect"]).unwrap();
-    println!("Output: {:?}", o);
+    let mut send = Command::cargo_bin("rzmq").unwrap()
+        .args(&["send", "--message", test_message, "--address", "tcp://127.0.0.1:5559", "--type", "PUSH", "connect"])
+        .spawn().unwrap();
 
-    listen.join();
+    std::thread::sleep(Duration::from_millis(200));
+
+    send.kill().unwrap();
+    listener.kill().unwrap();
+
+    let output = std::str::from_utf8(listener.wait_with_output().unwrap().stdout.as_slice()).unwrap().to_string();
+
+    println!("OUTPUT: {:?}", output);
+
+    assert!(output.contains(test_message));
 }
