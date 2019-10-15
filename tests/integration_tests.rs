213d5dacbe5b48d2f80ef74dfa08ee6cefdd6079
Grzegorz <grzegorz.bartoszek@thaumatec.com>
2019-10-15 10:55:45 +0200

    test for chat

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,5 +1,8 @@
 use std::process::{Command, Stdio};
 use assert_cmd::prelude::*;
+use std::io::{Write, Read, BufReader, BufRead};
+use std::thread::sleep;
+use std::time::Duration;
 
 
 fn test_push_pull_send_listen() {
@@ -44,8 +47,67 @@ fn test_pub_sub() {
     assert!(!output.contains(test_message_wo_topic));
 }
 
+fn test_pair_chat() {
+    let mut instance1 = Command::cargo_bin("rzmq").unwrap()
+        .args(&["chat", "--address", "tcp://127.0.0.1:5559", "--type", "PAIR", "bind"])
+        .stdout(Stdio::piped())
+        .stdin(Stdio::piped())
+        .spawn().unwrap();
+
+    let mut instance2 = Command::cargo_bin("rzmq").unwrap()
+        .args(&["chat", "--address", "tcp://127.0.0.1:5559", "--type", "PAIR", "connect"])
+        .stdout(Stdio::piped())
+        .stdin(Stdio::piped())
+        .spawn().unwrap();
+
+    let mut buffer = String::new();
+
+    sleep(Duration::from_secs(1));
+    {
+        let mut instance2_out = BufReader::new(instance2.stdout.as_mut().unwrap());
+        let mut instance1_out = BufReader::new(instance1.stdout.as_mut().unwrap());
+
+        instance1.stdin.as_mut().unwrap().write("Hi!\n".as_bytes());
+        instance1.stdin.as_mut().unwrap().write("How are you?\n".as_bytes());
+
+        sleep(Duration::from_secs(1));
+
+        instance2.stdin.as_mut().unwrap().write("\n".as_bytes()).unwrap();
+        instance2.stdin.as_mut().unwrap().write("\n".as_bytes()).unwrap();
+
+        sleep(Duration::from_secs(1));
+
+        instance2_out.read_line(&mut buffer);
+        instance2_out.read_line(&mut buffer);
+        println!("Output {}", buffer);
+        assert!(buffer.contains("Hi!"));
+
+        instance2.stdin.as_mut().unwrap().write("\n".as_bytes()).unwrap();
+
+        instance2_out.read_line(&mut buffer);
+        println!("Output {}", buffer);
+        assert!(buffer.contains("How are you?"));
+
+        instance2.stdin.as_mut().unwrap().write("I'm fine\n".as_bytes()).unwrap();
+
+        sleep(Duration::from_secs(1));
+
+        instance1.stdin.as_mut().unwrap().write("\n".as_bytes()).unwrap();
+        instance1_out.read_line(&mut buffer);
+        instance1_out.read_line(&mut buffer);
+        instance1_out.read_line(&mut buffer);
+        instance1_out.read_line(&mut buffer);
+        println!("Output {}", buffer);
+        assert!(buffer.contains("fine"));
+    }
+
+    instance1.kill();
+    instance2.kill();
+}
+
 #[test]
 fn integration_tests() {
     test_push_pull_send_listen();
     test_pub_sub();
+    test_pair_chat();
 }
