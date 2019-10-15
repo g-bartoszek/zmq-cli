58852e92f1c67c01f03739a749f4020d9b0e4515
Grzegorz <grzegorz.bartoszek@thaumatec.com>
2019-10-15 15:23:31 +0200

    test refactor

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -7,19 +7,77 @@ use nonblock::NonBlockingReader;
 use std::ops::{Deref, DerefMut};
 
 
+fn test_push_pull_send_listen() {
+    let test_message = "TEST MESSAGE 12345";
+
+    let mut listener = run_instance("listen --address tcp://127.0.0.1:5559 --type PULL bind").unwrap();
+    let _send = run_instance(format!("send --message {} --address tcp://127.0.0.1:5559 --type PUSH connect", test_message).as_str()).unwrap();
+
+    assert!(listener.wait_for_message(test_message).is_ok());
+}
+
+fn test_pub_sub() {
+    let test_message_wo_topic = "TEST MESSAGE1";
+    let test_message_with_topic = "TOPIC1 TEST MESSAGE2";
+
+    let mut listener = run_instance("listen --topic TOPIC1 --address tcp://127.0.0.1:5559 --type SUB bind").unwrap();
+    let _send1  = run_instance(format!("send --message {} --address tcp://127.0.0.1:5559 --type PUB connect",
+                                 test_message_wo_topic).as_str()).unwrap();
+
+    assert!(listener.wait_for_message( test_message_wo_topic).is_err());
+
+    let send2 = run_instance(format!("send --message {} --address tcp://127.0.0.1:5559 --type PUB connect",
+                         test_message_with_topic).as_str()).unwrap();
+
+    assert!(listener.wait_for_message( test_message_with_topic).is_ok());
+}
+
+fn test_pair_chat() {
+    let mut instance1 = run_instance("chat --address tcp://127.0.0.1:5559 --type PAIR bind").unwrap();
+    let mut instance2 = run_instance("chat --address tcp://127.0.0.1:5559 --type PAIR connect").unwrap();
+
+    instance1.stdin.as_mut().unwrap().write("Hi!\n".as_bytes());
+    instance1.stdin.as_mut().unwrap().write("How are you?\n".as_bytes());
+
+    sleep(Duration::from_secs(1));
+
+    instance2.stdin.as_mut().unwrap().write("\n".as_bytes()).unwrap();
+    assert!(instance2.wait_for_message(  "Hi!").is_ok());
+
+    instance2.stdin.as_mut().unwrap().write("\n".as_bytes()).unwrap();
+    assert!(instance2.wait_for_message(  "How are you?").is_ok());
+
+    instance2.stdin.as_mut().unwrap().write("I'm fine\n".as_bytes()).unwrap();
+
+    instance1.stdin.as_mut().unwrap().write("\n".as_bytes());
+    assert!(instance1.wait_for_message( "fine").is_ok());
+
+
+}
+
+#[test]
+fn integration_tests() {
+    test_push_pull_send_listen();
+    test_pub_sub();
+    test_pair_chat();
+}
+
 fn run_instance(args: &str) -> Result<Wrapper, String> {
     let args = args.split_whitespace();
 
     Command::cargo_bin("rzmq")
         .map_err(|e| e.to_string())
         .and_then(|mut command | {
-        command
-            .args(args)
-            .stdout(Stdio::piped())
-            .stdin(Stdio::piped())
-            .spawn()
-            .map_err(|e| e.to_string())
-    }).map(|c| Wrapper{ 0: c })
+            command
+                .args(args)
+                .stdout(Stdio::piped())
+                .stdin(Stdio::piped())
+                .spawn()
+                .map_err(|e| e.to_string())
+        }).map(|mut c| {
+        let reader = NonBlockingReader::from_fd(c.stdout.take().unwrap()).unwrap();
+        Wrapper{ 0: c, 1: reader}
+    })
 }
 
 fn wait_for_message(reader: &mut NonBlockingReader<ChildStdout>, message: &str) -> Result<(), &'static str> {
@@ -38,7 +96,7 @@ fn wait_for_message(reader: &mut NonBlockingReader<ChildStdout>, message: &str) 
     Err("Message not received")
 }
 
-struct Wrapper(Child);
+struct Wrapper(Child, NonBlockingReader<ChildStdout>);
 
 impl Drop for Wrapper {
     fn drop(&mut self) {
@@ -59,64 +117,21 @@ impl Deref for Wrapper {
     }
 }
 
-fn test_push_pull_send_listen() {
-    let test_message = "TEST MESSAGE 12345";
+impl Wrapper {
+    fn wait_for_message(&mut self, message: &str) -> Result<(), &'static str> {
+        let mut buffer = String::new();
 
-    let mut listener = run_instance("listen --address tcp://127.0.0.1:5559 --type PULL bind").unwrap();
-    let _send = run_instance(format!("send --message {} --address tcp://127.0.0.1:5559 --type PUSH connect", test_message).as_str()).unwrap();
+        for _ in 1..10 {
+            self.1.read_available_to_string(&mut buffer);
+            //println!("OUT: {}", buffer);
+            if buffer.contains(message) {
+                //println!("MATCHED: {}", buffer);
+                return Ok(());
+            }
+            sleep(Duration::from_millis(100));
+        }
 
-    let mut reader  = NonBlockingReader::from_fd(listener.stdout.take().unwrap()).unwrap();
-    assert!(wait_for_message(&mut reader, test_message).is_ok());
+        Err("Message not received")
+    }
 }
 
-fn test_pub_sub() {
-    let test_message_wo_topic = "TEST MESSAGE1";
-    let test_message_with_topic = "TOPIC1 TEST MESSAGE2";
-
-    let mut listener = run_instance("listen --topic TOPIC1 --address tcp://127.0.0.1:5559 --type SUB bind").unwrap();
-    let mut reader  = NonBlockingReader::from_fd(listener.stdout.take().unwrap()).unwrap();
-
-    let _send1  = run_instance(format!("send --message {} --address tcp://127.0.0.1:5559 --type PUB connect",
-                                 test_message_wo_topic).as_str()).unwrap();
-
-    assert!(!wait_for_message(&mut reader, test_message_wo_topic).is_ok());
-
-    let send2 = run_instance(format!("send --message {} --address tcp://127.0.0.1:5559 --type PUB connect",
-                         test_message_with_topic).as_str()).unwrap();
-
-    assert!(wait_for_message(&mut reader, test_message_with_topic).is_ok());
-}
-
-fn test_pair_chat() {
-    let mut instance1 = run_instance("chat --address tcp://127.0.0.1:5559 --type PAIR bind").unwrap();
-    let mut instance2 = run_instance("chat --address tcp://127.0.0.1:5559 --type PAIR connect").unwrap();
-
-    let mut reader1  = NonBlockingReader::from_fd(instance1.stdout.take().unwrap()).unwrap();
-    let mut reader2  = NonBlockingReader::from_fd(instance2.stdout.take().unwrap()).unwrap();
-
-
-    instance1.stdin.as_mut().unwrap().write("Hi!\n".as_bytes());
-    instance1.stdin.as_mut().unwrap().write("How are you?\n".as_bytes());
-
-    sleep(Duration::from_secs(1));
-
-    instance2.stdin.as_mut().unwrap().write("\n".as_bytes()).unwrap();
-    assert!(wait_for_message(&mut reader2,  "Hi!").is_ok());
-
-    instance2.stdin.as_mut().unwrap().write("\n".as_bytes()).unwrap();
-    assert!(wait_for_message(&mut reader2,  "How are you?").is_ok());
-
-    instance2.stdin.as_mut().unwrap().write("I'm fine\n".as_bytes()).unwrap();
-
-    instance1.stdin.as_mut().unwrap().write("\n".as_bytes());
-    assert!(wait_for_message(&mut reader1,  "fine").is_ok());
-
-
-}
-
-#[test]
-fn integration_tests() {
-    test_push_pull_send_listen();
-    test_pub_sub();
-    test_pair_chat();
-}
