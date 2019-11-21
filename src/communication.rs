d0c8f94cea6334f287f114d85e83b2580cb458fc
Grzegorz <grzegorz.bartoszek@thaumatec.com>
2019-11-21 22:40:03 +0100

    chat module extracted

--- src/communication.rs
+++ src/communication.rs
@@ -2,7 +2,6 @@
 use std::time::Duration;
 use std::error::Error;
 use std::thread::sleep;
-use zmq::{Context, Socket};
 use crate::socket::{SocketParameters, create_socket};
 
 pub fn listen(parameters: SocketParameters) -> Result<(), Box<dyn Error>> {
@@ -27,34 +26,4 @@ pub fn send(parameters: SocketParameters, message: &str) -> Result<(), Box<dyn E
     Ok(())
 }
 
-pub struct Chat {
-    ctx: Context,
-    socket: Socket
-}
-
-impl Chat {
-    pub fn new(parameters: &SocketParameters) -> Result<Self, Box<dyn Error>> {
-        let ctx = zmq::Context::new();
-        let socket = create_socket(&ctx, &parameters)?;
-        socket.set_rcvtimeo(100)?;
-
-        Ok(Self { ctx, socket })
-    }
-
-    pub fn send(&self, message: &str) -> Result<(), Box<dyn Error>> {
-       self.socket.send(message, 0)?;
-       Ok(())
-    }
-
-    pub fn receive(&self) -> Result<Vec<String>, Box<dyn Error>> {
-        let message = self.socket.recv_multipart(0)?;
-        let result = message
-            .iter()
-            .map(|part | {
-                String::from_utf8(part.to_vec())
-            }).collect::<Result<Vec<_>, _>>()?;
-        Ok(result)
-
-    }
-}
 
